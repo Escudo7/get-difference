075bf791129bcfcbf5daca2466234cf1d1012634
Escudo7 <permswolf@ys.ru>
2019-07-20 21:26:34 +0500

    refactoring

--- src/getdiff.php
+++ src/getdiff.php
@@ -2,43 +2,42 @@
 
 namespace Project\getDiff;
 
+use Funct\Collection;
+
 function getDiff($date1, $date2)
 {
-    $keys1 = array_keys($date1);
-    $keys2 = array_keys($date2);
-    $diffPartOne = array_reduce($keys1, function ($acc, $key) use ($date1, $date2) {
-        if (gettype($date1[$key]) == 'boolean') {
-            $value1 = $date1[$key] === true ? 'true' : 'false';
-        } else {
-            $value1 = $date1[$key];
-        }
-        if (array_key_exists($key, $date2)) {
-            if (gettype($date2[$key]) == 'boolean') {
-                $value2 = $date2[$key] === true ? 'true' : 'false';
-            } else {
-                $value2 = $date2[$key];
-            }
+    $keys = Collection\union(array_keys($date1), array_keys($date2));
+    $diff = array_reduce($keys, function ($acc, $key) use ($date1, $date2) {
+        if (isset($date1[$key]) && isset($date2[$key])) {
             if ($date1[$key] === $date2[$key]) {
-                $str = "    {$key}: {$value1}\n";
+                $value = isBoolean($date2[$key]) ? convertBoolToStr($date2[$key]) : $date2[$key];
+                $newStr = "    {$key}: {$value}\n";
             } else {
-                $str = "  - {$key}: {$value1}\n  + {$key}: {$value2}\n";
+                $value1 = isBoolean($date1[$key]) ? convertBoolToStr($date1[$key]) : $date1[$key];
+                $value2 = isBoolean($date2[$key]) ? convertBoolToStr($date2[$key]) : $date2[$key];
+                $newStr = "  - {$key}: {$value1}\n  + {$key}: {$value2}\n";
             }
         } else {
-            $str = "  - {$key}: {$date1[$key]}\n";
-        }
-        return "{$acc}{$str}";
-    }, '');
-    $diffPartTwo = array_reduce($keys2, function ($acc, $key) use ($date1, $date2) {
-        if (!array_key_exists($key, $date1)) {
-            if (gettype($date2[$key]) == 'boolean') {
-                $value2 = $date2[$key] === true ? 'true' : 'false';
+            if (isset($date1[$key])) {
+                $value = isBoolean($date1[$key]) ? convertBoolToStr($date1[$key]) : $date1[$key];
+                $newStr = "  - {$key}: {$value}\n";
             } else {
-                $value2 = $date2[$key];
+                $value = isBoolean($date2[$key]) ? convertBoolToStr($date2[$key]) : $date2[$key];
+                $newStr = "  + {$key}: {$value}\n";
             }
-            $str = "  + {$key}: {$value2}";
-            return "{$acc}{$str}";
         }
-        return $acc;
-    }, $diffPartOne);
-    return "{\n{$diffPartTwo}\n}\n";
+        return "{$acc}{$newStr}";
+    }, "");
+    $result = "{\n{$diff}}\n";
+    return $result;
+}
+
+function isBoolean($date)
+{
+    return gettype($date) === 'boolean';
+}
+
+function convertBoolToStr($date)
+{
+    return $date === true ? 'true' : 'false';
 }
