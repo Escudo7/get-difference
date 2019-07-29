ca485b1fe06cceeddcd12d31252c9e1f153e154c
Escudo7 <permswolf@ys.ru>
2019-07-29 22:53:41 +0500

    refactoring

--- src/getDiff.php
+++ src/getDiff.php
@@ -3,10 +3,13 @@
 namespace Project\getDiff;
 
 use Funct\Collection;
+use function Project\getData\getData;
 use function Project\buildAst\buildAst;
 
-function getDiff($data1, $data2, $format = "pretty")
+function getDiff($pathToFile1, $pathToFile2, $format)
 {
+    $data1 = getData($pathToFile1);
+    $data2 = getData($pathToFile2);
     $ast = buildAst($data1, $data2);
     $render = "Project\\renderer\\{$format}\\render";
     return $render($ast);
--- tests/genDiffTest.php
+++ tests/genDiffTest.php
@@ -9,17 +9,8 @@ class MyTest extends TestCase
 {
     public function testPrettyDiffFlat()
     {
-        $file1 = [
-            "host" => "hexlet.io",
-            "timeout" => 50,
-            "proxy" => "123.234.53.22"
-        ];
-
-        $file2 = [
-            "timeout" => 20,
-            "verbose" => true,
-            "host" => "hexlet.io"
-        ];
+        $pathToFile1 = __DIR__ . "/testsFiles/beforePrettyFlat.json";
+        $pathToFile2 = __DIR__ . "/testsFiles/afterPrettyFlat.json";
         $result = "{
     host: hexlet.io
   - timeout: 50
@@ -27,47 +18,13 @@ class MyTest extends TestCase
   - proxy: 123.234.53.22
   + verbose: true
 }\n";
-        $this->assertEquals($result, getDiff($file1, $file2, 'pretty'));
+        $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'pretty'));
     }
 
     public function testPrettyDiffNested()
     {
-        $file1 = [
-            "host" => "hexlet.io",
-            "timeout" => 50,
-            "proxy" => "123.234.53.22",
-            "unmodifiedArray" => [
-                "key1" => "value1",
-                "key2" => "value2"
-            ],
-            "modifiedArray" => [
-                "big" => 'bag',
-                'cat' => 'dog'
-            ]
-        ];
-
-        $file2 = [
-            "timeout" => 20,
-            "verbose" => true,
-            "host" => "hexlet.io",
-            "unmodifiedArray" => [
-                "key1" => "value1",
-                "key2" => "value2"
-            ],
-            "modifiedArray" => [
-                "big" => "head",
-                "newCat" => [
-                    "one" => "pers",
-                    "two" => "sfinks"
-                    ],
-                "cat" => "dog"
-            ],
-            "newArray" => [
-                "newKey1" => "value1",
-                "newKey2" => "value2"
-                ]
-        ];
-
+        $pathToFile1 = __DIR__ . "/testsFiles/beforePrettyNested.json";
+        $pathToFile2 = __DIR__ . "/testsFiles/afterPrettyNested.json";
         $result = "{
     host: hexlet.io
     unmodifiedArray: {
@@ -92,64 +49,23 @@ class MyTest extends TestCase
         newKey2: value2
     }
 }\n";
-        $this->assertEquals($result, getDiff($file1, $file2, 'pretty'));
+        $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'pretty'));
     }
 
     public function testPlainDiffFlat()
     {
-        $file1 = [
-            "host" => "hexlet.io",
-            "timeout" => 50,
-            "proxy" => "123.234.53.22"
-        ];
-
-        $file2 = [
-            "timeout" => 20,
-            "verbose" => true,
-            "host" => "hexlet.io"
-        ];
+        $pathToFile1 = __DIR__ . "/testsFiles/beforePlainFlat.yml";
+        $pathToFile2 = __DIR__ . "/testsFiles/afterPlainFlat.yml";
         $result = "Property 'timeout' was changed. From '50' to '20'
 Property 'proxy' was removed
 Property 'verbose' was added with value: 'true'\n";
-        $this->assertEquals($result, getDiff($file1, $file2, 'plain'));
+        $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'plain'));
     }
 
     public function testPlainDiffNestes()
     {
-        $file1 = [
-            "common" => [
-                "setting1" => "Value 1",
-                "setting2" => "200",
-                "setting3" => true,
-                "setting6" => [
-                    "key" => "value"
-                ]
-            ],
-            "group1" => [
-                "baz" => "bas",
-                "foo" => "bar"
-            ],
-            "group2" => [
-                "abc" => "12345"
-            ]
-        ];
-        $file2 = [
-            "common" => [
-                "setting1" => "Value 1",
-                "setting3" => true,
-                "setting4" => "blah blah",
-                "setting5" => [
-                    "key5" => "value5"
-                ]
-            ],
-            "group1" => [
-                "foo" => "bar",
-                "baz" => "bars"
-            ],
-            "group3" => [
-                "fee" => "100500"
-            ]
-        ];
+        $pathToFile1 = __DIR__ . "/testsFiles/beforePlainNested.yml";
+        $pathToFile2 = __DIR__ . "/testsFiles/afterPlainNested.yml";
         $result = "Property 'common.setting2' was removed
 Property 'common.setting6' was removed
 Property 'common.setting4' was added with value: 'blah blah'
@@ -157,6 +73,6 @@ Property 'common.setting5' was added with value: 'complex value'
 Property 'group1.baz' was changed. From 'bas' to 'bars'
 Property 'group2' was removed
 Property 'group3' was added with value: 'complex value'\n";
-        $this->assertEquals($result, getDiff($file1, $file2, 'plain'));
+        $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'plain'));
     }
 }
