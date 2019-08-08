d58692407799378ab2c6da2f580d475d6578ad73
Escudo7 <permswolf@ys.ru>
2019-08-08 22:00:10 +0500

    refactoring tests

--- tests/genDiffTest.php
+++ tests/genDiffTest.php
@@ -11,8 +11,8 @@ class DiffTest extends TestCase
     {
         $pathToFile1 = __DIR__ . "/testsFiles/flatBefore.json";
         $pathToFile2 = __DIR__ . "/testsFiles/flatAfter.json";
-        $pathToResult = __DIR__ . "/testsFiles/resultFlatPretty.json";
-        $result = json_decode(file_get_contents($pathToResult));
+        $pathToResult = __DIR__ . "/testsFiles/resultFlatPretty";
+        $result = file_get_contents($pathToResult);
 
         $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'pretty'));
     }
@@ -21,8 +21,8 @@ class DiffTest extends TestCase
     {
         $pathToFile1 = __DIR__ . "/testsFiles/nestedBefore.yml";
         $pathToFile2 = __DIR__ . "/testsFiles/nestedAfter.yml";
-        $pathToResult = __DIR__ . "/testsFiles/resultNestedPretty.json";
-        $result = json_decode(file_get_contents($pathToResult));
+        $pathToResult = __DIR__ . "/testsFiles/resultNestedPretty";
+        $result = file_get_contents($pathToResult);
         
         $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'pretty'));
     }
@@ -31,8 +31,8 @@ class DiffTest extends TestCase
     {
         $pathToFile1 = __DIR__ . "/testsFiles/flatBefore.json";
         $pathToFile2 = __DIR__ . "/testsFiles/flatAfter.json";
-        $pathToResult = __DIR__ . "/testsFiles/resultFlatPlain.json";
-        $result = json_decode(file_get_contents($pathToResult));
+        $pathToResult = __DIR__ . "/testsFiles/resultFlatPlain";
+        $result = file_get_contents($pathToResult);
 
         $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'plain'));
     }
@@ -41,8 +41,8 @@ class DiffTest extends TestCase
     {
         $pathToFile1 = __DIR__ . "/testsFiles/nestedBefore.yml";
         $pathToFile2 = __DIR__ . "/testsFiles/nestedAfter.yml";
-        $pathToResult = __DIR__ . "/testsFiles/resultNestedPlain.json";
-        $result = json_decode(file_get_contents($pathToResult));
+        $pathToResult = __DIR__ . "/testsFiles/resultNestedPlain";
+        $result = file_get_contents($pathToResult);
 
         $this->assertEquals($result, getDiff($pathToFile1, $pathToFile2, 'plain'));
     }
